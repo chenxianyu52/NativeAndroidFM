19ba4577a8c1456aeda39a2180d51a195959ae7a
陈贤煜 <chenxianyu@chenxianyudeMacBook-Pro.local>
2021-11-03 14:08:00 +0800

    init

---
+++ app/src/main/java/com/xianyu/androidfm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.xianyu.androidfm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
