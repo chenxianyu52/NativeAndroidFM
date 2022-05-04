e7c3729c83070b6d70d73d43c81c24a34bf785e7
chenxianyu52 <380286681@qq.com>
2022-05-04 23:37:44 +0800

    将lib_androidlibrary抽出去了

--- lib_androidlibrary/src/main/java/com/xianyu/androidlibrary/pull/OnNewLoadMoreListener.kt
+++
@@ -1,5 +0,0 @@
-package com.xianyu.androidlibrary.pull
-
-import com.aspsine.swipetoloadlayout.OnLoadMoreListener
-
-interface OnNewLoadMoreListener : OnLoadMoreListener
--- lib_androidlibrary/src/main/java/com/xianyu/androidlibrary/pull/OnNewRefreshListener.kt
+++
@@ -1,5 +0,0 @@
-package com.xianyu.androidlibrary.pull
-
-import com.aspsine.swipetoloadlayout.OnRefreshListener
-
-interface OnNewRefreshListener : OnRefreshListener
